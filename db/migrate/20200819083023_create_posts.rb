5324a28ea98abf6e6dad12f79449417f3d626d82
thiemmal <takuto_0914@icloud.com>
2020-08-19 17:58:47 +0900

    posts_migrate

--- db/migrate/20200819083023_create_posts.rb
+++ db/migrate/20200819083023_create_posts.rb
@@ -1,9 +1,11 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
-      t.string :subject
+      t.string :subject, null: false
       t.text :content
       t.string :image
+      t.datetime :start_time
+      
 
       t.timestamps
     end
